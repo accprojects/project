a3bd18d80da7505ffb574c6806d7acbc296f1b2f
admin <lianatagirova598@gmail.com>
2018-01-20 18:50:31 +0500

    mod settings

--- b_project/settings.py
+++ b_project/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'all_files',
-    'bootstrap3',
+    'all_files', 
 ]
 
 SITE_ID = 1
